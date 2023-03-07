f4d6c394b96be16ad34cef246c44748a71ebdd5e
Anzes <jakob.anzengruber@gmx.at>
2023-03-07 11:14:24 +0100

    Initial commit

---
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+
+    Animator anim;
+    GameObject player;
+    PlayerHealth playerHealth;
+    //EnemyHealth enemyHealth;
+    bool playerInRange;
+    float timer;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator> ();
+        player = GameObject.FindGameObjectWithTag ("Player");
+//        enemyHealth = GetComponent<EnemyHealth>();
+        playerHealth = player.GetComponent<PlayerHealth> ();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = false;
+        }
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (playerInRange && timer >= timeBetweenAttacks)
+        if (timer >= timeBetweenAttacks && playerInRange /* && enemyHealth.currentHealth > 0 */)
+
+            {
+                Attack ();
+        }
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger ("PlayerDead");
+        }
+    }
+
+    void Attack()
+    {
+        timer = 0f;
+        if (playerHealth.currentHealth > 0)
+        {
+            playerHealth.TakeDamage (attackDamage);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed = 6f;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLength = 100f;
+    Camera mainCam;
+
+    private void Awake() {
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        floorMask = LayerMask.GetMask("Floor");
+        mainCam = Camera.main;
+    }
+
+    private void FixedUpdate() {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Move(h, v);
+        Turning();
+        Animating(h, v);
+    }
+
+    void Move(float h, float v) {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turning() {
+        Ray camRay = mainCam.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(camRay, out hit, camRayLength, floorMask)) {
+            Vector3 playerToMouse = hit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+    void Animating(float h, float v) {
+        bool walking = h != 0f || v != 0f;
+        anim.SetBool("IsWalking", true);
+    }
+}
